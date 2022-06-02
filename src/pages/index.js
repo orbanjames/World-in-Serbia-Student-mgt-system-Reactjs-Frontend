f32ab16f198de13c5879342e57561a9f32636951
orbanjames <96356809+orbanjames@users.noreply.github.com>
2022-06-02 05:32:09 +0200

    subsequent commits and updates

---
+++ src/pages/index.js
@@ -0,0 +1,30 @@
+import Home from './HomePage'
+import Students from './StudentsPage'
+import About from './AboutPage'
+import Error from './ErrorPage'
+import Products from './ProductsPage'
+import SingleProduct from './SingleProductPage'
+import Cart from './CartPage'
+import Checkout from './CheckoutPage'
+import PrivateRoute from './PrivateRoute'
+import Universities from './UniversitiesPage'
+import Faculties from './FacultiesPage'
+import AddStudents from './AddStudentPage'
+import AddFaculty from './AddFacultyPage'
+import AddUniversity from './AddUniversityPage'
+export {
+  Home,
+  Students,
+  About,
+  Error,
+  Cart,
+  Products,
+  SingleProduct,
+  Checkout,
+  PrivateRoute,
+  Universities,
+  Faculties,
+  AddStudents,
+  AddFaculty,
+  AddUniversity,
+}
