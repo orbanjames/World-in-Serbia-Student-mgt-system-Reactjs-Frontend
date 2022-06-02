9a9e96a0a0319bdc971c38de581499868fbc8c82
orbanjames <96356809+orbanjames@users.noreply.github.com>
2022-06-02 05:15:32 +0200

    subsequent commits and updates

--- src/App.js
+++ src/App.js
@@ -1,35 +1,68 @@
-
-import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import FooterComponent from "./components/FooterComponent";
-import HeaderComponent from "./components/HeaderComponent";
-import { ListStudentComponent } from "./components/ListStudentComponent";
-import AddStudentComponent from "./components/AddStudentComponent";
-import ViewStudentComponent from './components/ViewStudentComponent';
-import { ListUniversitiesComponent } from "./components/ListUniversitiesComponent";
-import AddUniversityComponent from "./components/AddUniversityComponent";
+import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import AddStudentComponent from './components/AddStudentComponent'
+import ViewStudentComponent from './components/ViewStudentComponent'
+import AddUniversityComponent from './components/AddUniversityComponent'
+import { Navbar, Sidebar, Footer } from './components'
+import {
+  Home,
+  About,
+  Products,
+  SingleProduct,
+  Error,
+  Cart,
+  Checkout,
+  Students,
+  Faculties,
+  Universities,
+  PrivateRoute,
+  AddStudents,
+  AddFaculty,
+  AddUniversity,
+} from './pages'
 
 function App() {
   return (
-    <div>
-      <Router>
-        <HeaderComponent />
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<ListStudentComponent />}></Route>
-            <Route path="/students" element={<ListStudentComponent />}></Route>{" "}
-            <Route path="/universities" element={<ListUniversitiesComponent />}></Route>{" "}
-            <Route path="/add-student" element={<AddStudentComponent />}></Route>
-            <Route path="/edit-student/:id" element={<AddStudentComponent />}></Route>
-            <Route path="/add-university" element={<AddUniversityComponent />}></Route>
-            <Route path="/edit-university/:id" element={<AddUniversityComponent />}></Route>
-            <Route path="/view-student/:id" element={<ViewStudentComponent />}></Route>
-          </Routes>
-        </div>
-        <FooterComponent />
-      </Router>
-    </div>
-  );
+    <Router>
+      <Navbar />
+      <Sidebar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='about' element={<About />}></Route>
+        <Route path='cart' element={<Cart />}></Route>
+        <Route path='products' element={<Products />}></Route>
+        <Route path='Products/:id' element={<SingleProduct />}></Route>
+        <Route
+          path='checkout'
+          element={
+            <PrivateRoute>
+              <Checkout />
+            </PrivateRoute>
+          }
+        ></Route>
+        <Route path='students' element={<Students />}></Route>
+        <Route path='/universities' element={<Universities />}></Route>
+        <Route path='/faculties' element={<Faculties />}></Route>
+        <Route path='/add-student' element={<AddStudents />}></Route>
+        <Route
+          path='/edit-student/:id'
+          element={<AddStudentComponent />}
+        ></Route>
+        <Route path='/add-university' element={<AddUniversity />}></Route>
+        <Route path='/add-faculty' element={<AddFaculty />}></Route>
+        <Route
+          path='/edit-university/:id'
+          element={<AddUniversityComponent />}
+        ></Route>
+        <Route
+          path='/view-student/:id'
+          element={<ViewStudentComponent />}
+        ></Route>
+        <Route path='*' element={<Error />}></Route>
+      </Routes>
+      <Footer />
+    </Router>
+  )
 }
 
-export default App;
+export default App
